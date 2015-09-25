ea27238b822ae707a9de30f5a38bdc7077ffe324
fitosegrera <fitosegrera@gmail.com>
2015-09-25 17:28:10 +0000

    corrections

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var connect = require('connect')
 var serveStatic = require('serve-static')
 var exec = require('child_process').exec
-exec('raspistill -w 320 -h 240 -o public/image.jpg -t 20000 -tl 100')
+exec('raspistill -w 1280 -h 720 -o public/image.jpg -t 20000 -tl 1000')
 connect().use(serveStatic(__dirname + '/public')).listen(8080, function(){
 	console.log("connected on port: 8080")
 })
